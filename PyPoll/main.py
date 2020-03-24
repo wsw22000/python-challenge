c121e329d54188e053f43fb7fb9216fce7ef3e43
Shackleton 22000 <wsw22000@yahoo.com>
2020-03-24 08:55:10 -0500

    Submitting python-challenge with output working

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,6 +18,11 @@ with open(csvpath) as csvfile:
         total_votes_cast += 1
         all_named_votes.append(row[2])
 
+    import pandas as pd
+
+    # df = pd.DataFrame({'A':all_named_votes})
+    # candidate_list = df['A'].unique()
+    
     candidate_list = list(set(all_named_votes))
 
     votes_for = 0
@@ -25,14 +30,36 @@ with open(csvpath) as csvfile:
     winner = ""
 
     for candidate in candidate_list:
-        for row in csvreader: 
-            if candidate = row[2]        
+        for vote in all_named_votes: 
+            if candidate == vote:       
                 votes_for += 1
 
         votes_by_candidate[candidate] = votes_for       
 
-            if votes_for > winning_vote_count:
-                winner = candidate
-                winning_vote_count = votes_for
+        if votes_for > winning_vote_count:
+            winner = candidate
+            winning_vote_count = votes_for
     
         votes_for = 0
+
+print('Election Results')
+print('--------------------')
+print(f'Total Votes: {total_votes_cast}')
+print('--------------------')
+for candidate in sorted(votes_by_candidate, key=votes_by_candidate.get, reverse=True):
+    print(f'{candidate}: {round(votes_by_candidate[candidate]/total_votes_cast*100,3)}% ({votes_by_candidate[candidate]})')
+print('--------------------')
+print(f'Winner: {winner}')    
+print('--------------------')
+
+output_path = open('election_results.txt', 'w')
+
+output_path.write(f'Election Results\n')
+output_path.write(f'--------------------\n')
+output_path.write(f'Total Votes: {total_votes_cast}\n')
+output_path.write(f'--------------------\n')
+for candidate in sorted(votes_by_candidate, key=votes_by_candidate.get, reverse=True):
+    output_path.write(f'{candidate}: {round(votes_by_candidate[candidate]/total_votes_cast*100,3)}% ({votes_by_candidate[candidate]})\n')
+output_path.write('--------------------\n')
+output_path.write(f'Winner {winner}\n')
+output_path.write('--------------------\n')
